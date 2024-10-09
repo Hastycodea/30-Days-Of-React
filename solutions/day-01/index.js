39c1a79d56da7d4eba29cbaa8d8040f5c87e7993
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-09 09:26:07 +0300

    first

---
+++ solutions/day-01/index.js
@@ -0,0 +1,75 @@
+// console.log('Welcome to 30 days of Js');
+
+// const fruits = ['Mangoes', 'Banana', 'Apple'];
+// console.log(fruits)
+
+
+// const arr = [
+//     'Asabeneh',
+//     250,
+//     true,
+//     { country: 'Finland', city: 'Helsinki' },
+//     { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] },
+// ] // arr containing different data types
+// console.log(arr[3].country)
+
+
+let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+console.log('Accessing letter J: ' + charsInJavaScript[0]);
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+let txt = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = txt.split(' ')
+
+console.log(words)
+// the text has special characters think how you can just get only the words
+// ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
+
+let word = '"I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"'
+const wordsResult = word.match(/[a-zA-Z]+/g); // g is global flag to find all the matches
+const wordsResult2 = word.match(/\b\w+\b/g); // b is boundary
+console.log(wordsResult);
+console.log('The index of React is: ' + wordsResult.indexOf('React'));
+console.log(wordsResult2);
+
+let neno = 'Python'
+let index = wordsResult.indexOf(neno);
+index != -1 ? console.log('found!') : console.log('not found!');
+
+let numbers = [1, 2, 3, 4, 5, 6]
+console.log(numbers.splice(3, 3, 7, 8, 9)) // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
+console.log(numbers)
+
+let figures = [1, 2, 9, 3, 4, 5];
+// figures.push(6); 
+// figures.pop();
+// figures.shift();
+// figures.unshift(0);
+// figures.sort();
+// figures.reverse();
+
+console.log(figures)
+
+
+const firstNums = [1, 2, 3]
+const secondNums = [1, 4, 9]
+
+const arrayOfArray = [
+    [1, 2, 3],
+    [1, 2, 3],
+]
+console.log(arrayOfArray) // [1, 2, 3]
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+const fullStack = [frontEnd, backEnd]
+console.log(fullStack) // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+console.log(fullStack.length) // 2
+console.log(fullStack[0]) // ["HTML", "CSS", "JS", "React", "Redux"]
+console.log(fullStack[1]) // ["Node", "Express", "MongoDB"]
--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -1,1 +1,43 @@
 console.log('30-days-of-react')
+
+// let names = Array(9)
+let names = []
+console.log(names);
+
+let countries = ['Kenya', 'Uganda', 'Malawi', 'Zambia', 'Jamaica'];
+console.log(countries)
+
+console.log(countries.length)
+
+console.log(countries[0])
+console.log(countries[2])
+console.log(countries[4])
+
+const mixedDataTypes = [
+    'June',
+    12,
+    true,
+    {firstName: 'Andreh', secondName: 'Andereh', nickName: 'Juju'},
+]
+
+console.log(mixedDataTypes)
+
+const itCompanies = ["Facebook", 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+console.log(itCompanies);
+
+console.log(itCompanies.length)
+
+let n = itCompanies.length;
+
+console.log(itCompanies[0])
+console.log(itCompanies[(n - 1) / 2])
+console.log(itCompanies[n - 1])
+
+itCompanies.map(it => console.log(it));
+
+itCompanies.map(it => console.log(it.toUpperCase()))
+
+console.log(itCompanies.toString());
+
+let index = itCompanies.indexOf('Safaricom')
+index > -1 ? console.log('Found!') : console.log('Not Found!');
